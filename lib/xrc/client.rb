9657a25ffb7e29918a26ecc53ef19b75992f01f4
Ryo Nakamura <r7kamura@gmail.com>
2014-05-08 20:11:24 +0900

    Implement PLAIN SASL authentication

--- lib/xrc/client.rb
+++ lib/xrc/client.rb
@@ -1,4 +1,5 @@
 require "active_support/core_ext/string/indent"
+require "base64"
 
 module Xrc
   class Client
@@ -32,12 +33,17 @@ module Xrc
             feature.each_element("mechanism") do |mechanism|
               mechanisms << mechanism.text
             end
+            on_mechanisms_received
           else
             features[feature.name] = feature.namespace
           end
         end
       when element.name == "proceed" && element.namespace == TLS_NAMESPACE
         change_socket
+      when element.name == "success" && element.namespace == SASL_NAMESPACE
+        on_authentication_succeeded
+      when element.name == "failure" && element.namespace == SASL_NAMESPACE
+        on_authentication_failed
       end
     end
 
@@ -46,6 +52,43 @@ module Xrc
     end
 
     private
+
+    def on_authentication_succeeded
+      start
+    end
+
+    def on_authentication_failed
+      raise NotImplementedError
+    end
+
+    def authenticate
+      case
+      when mechanisms.include?("PLAIN")
+        element = REXML::Element.new("auth")
+        element.add_namespace(SASL_NAMESPACE)
+        element.attributes["mechanism"] = "PLAIN"
+        element.text = Base64.strict_encode64(plain_credentials)
+        post(element)
+      else
+        raise NotImplementedError
+      end
+    end
+
+    def plain_credentials
+      "#{jid}\x00#{jid.node}\x00#{password}"
+    end
+
+    def on_mechanisms_received
+      authenticate if has_password?
+    end
+
+    def has_password?
+      !!password
+    end
+
+    def password
+      options[:password]
+    end
 
     def connect
       socket
