16a77f6d58d450f22c788f2f1ad4eaffc9f122ca
ITO Nobuaki <daydream.trippers@gmail.com>
2014-09-04 22:20:25 +0900

    Fix room_name of invite message

--- lib/xrc/messages/invite.rb
+++ lib/xrc/messages/invite.rb
@@ -2,7 +2,7 @@ module Xrc
   module Messages
     class Invite < Base
       def room_name
-        @element["name/text()"].to_s
+        @element.elements["//name/text()"].to_s
       end
     end
   end
